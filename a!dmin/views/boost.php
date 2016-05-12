4f86fe5393ac05a5fab30123d388651495944b0e
eiiikunkot <chezara40@gmail.com>
2016-05-12 09:12:19 +0300

    fix save

--- a!dmin/views/boost.php
+++ a!dmin/views/boost.php
@@ -1,4 +1,4 @@
-<form method="post" action="/services/boost.php/?action=text&game=<?php echo $game?>"> 
+<form method="post" action="/a!dmin/services/boost.php/?action=text&game=<?php echo $game?>"> 
 	<textarea name="text" style="width: 100%; min-height: 450pt" ><?php echo !empty($text) ? $text : 'Введите текст страницы'?></textarea><br>
 	<input type="submit" class="btn btn-info" value="Сохранить"> 
 </form>
